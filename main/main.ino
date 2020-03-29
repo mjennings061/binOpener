41cb07ee7dedb1cfe53f67d173b3642ee48f2aa1
Michael Jennings <mjennings061@gmail.com>
2020-03-29 13:23:00 +0100

    Create ultrasonic distance and put in a function

--- main/main.ino
+++ main/main.ino
@@ -9,43 +9,51 @@
         Echo: Echo (OUTPUT) - Pin 12
         GND: GND
  */
- 
-int trigPin = 11;    // Trigger
-int echoPin = 12;    // Echo
-long duration, cm, inches;
+
+// Turn on debug statements to the serial output
+#define DEBUG 1// enables debug serial prints
+#ifdef DEBUG
+#define DEBUG_PRINT(x)      Serial.print (x)
+#define DEBUG_PRINTLN(x)  Serial.println (x)
+#else
+#define DEBUG_PRINT(x)
+#define DEBUG_PRINTLN(x)
+#endif
+
+#define TRIG_PIN 11    // Trigger
+#define ECHO_PIN 12    // Echo
  
 void setup() {
   //Serial Port begin
   Serial.begin (9600);
   //Define inputs and outputs
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
 }
  
 void loop() {
+  long dist = distInCm(); //calculate distance in centimeters
+  Serial.print(dist);
+  Serial.print("cm\n");
+  delay(250);
+}
+
+long distInCm(void){
   // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
   // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
-  digitalWrite(trigPin, LOW);
+  digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(5);
-  digitalWrite(trigPin, HIGH);
+  digitalWrite(TRIG_PIN, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+  digitalWrite(TRIG_PIN, LOW);
  
   // Read the signal from the sensor: a HIGH pulse whose
   // duration is the time (in microseconds) from the sending
   // of the ping to the reception of its echo off of an object.
-  pinMode(echoPin, INPUT);
-  duration = pulseIn(echoPin, HIGH);
+  pinMode(ECHO_PIN, INPUT);
+  long duration = pulseIn(ECHO_PIN, HIGH);
  
   // Convert the time into a distance
-  cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
-  inches = (duration/2) / 74;   // Divide by 74 or multiply by 0.0135
-  
-  Serial.print(inches);
-  Serial.print("in, ");
-  Serial.print(cm);
-  Serial.print("cm");
-  Serial.println();
-  
-  delay(250);
+  long cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
+  return cm;
 }
